d4c5d57049164080e2f12bbfea4e0b62c2c1a3ad
Yago <yagolopeslazaro@gmail.com>
2018-12-13 11:52:59 -0200

    Projeto desenvolvido com a turma de pedagogia

---
+++ Assets/Scripts/responder.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class responder : MonoBehaviour {
+    private int idTema;
+
+    public Text pergunta;
+    public Text respostaA;
+    public Text respostaB;
+    public Text respostaC;
+    public Text respostaD;
+    public Text infoRespostas;
+
+    public string[] perguntas;
+    public string[] alternativaA;
+    public string[] alternativaB;
+    public string[] alternativaC;
+    public string[] alternativaD;
+    public string[] corretas;
+
+    private int idPergunta;
+    private float acertos;
+    private float questoes;
+    private float media;
+    private int notaFinal;
+    // Use this for initialization
+    void Start() {
+        idTema = PlayerPrefs.GetInt("idTema");
+        idPergunta = 0;
+        questoes = perguntas.Length;
+
+        pergunta.text = perguntas[idPergunta];
+        respostaA.text = alternativaA[idPergunta];
+        respostaB.text = alternativaB[idPergunta];
+        respostaC.text = alternativaC[idPergunta];
+        respostaD.text = alternativaD[idPergunta];
+
+        infoRespostas.text = "Respondendo " + (idPergunta + 1).ToString() + " de " + questoes.ToString() + " perguntas.";
+
+    }
+
+    public void resposta(string alternativa)
+    {
+        if (alternativa == "A")
+        {
+            if (alternativaA [idPergunta] == corretas[idPergunta])
+            {
+                acertos += 1;
+            }
+
+        }
+
+       else if (alternativa == "B")
+        {
+            if (alternativaB[idPergunta] == corretas[idPergunta])
+            {
+                acertos += 1;
+            }
+
+        }
+       else if (alternativa == "C")
+        {
+            if (alternativaC[idPergunta] == corretas[idPergunta])
+            {
+                acertos += 1;
+            }
+
+        }
+
+       else if (alternativa == "D")
+        {
+            if (alternativaD[idPergunta] == corretas[idPergunta])
+            {
+                acertos += 1;
+            }
+            
+        }
+
+        proximaPergunta();
+
+    }
+    void proximaPergunta()
+    {
+        idPergunta += 1;
+
+        if (idPergunta <= (questoes - 1))
+        {
+
+            pergunta.text = perguntas[idPergunta];
+            respostaA.text = alternativaA[idPergunta];
+            respostaB.text = alternativaB[idPergunta];
+            respostaC.text = alternativaC[idPergunta];
+            respostaD.text = alternativaD[idPergunta];
+
+            infoRespostas.text = "Respondendo " + (idPergunta + 1).ToString() + " de " + questoes.ToString() + " perguntas.";
+        }
+        else
+        {   
+
+
+            Application.LoadLevel("notaFinal");
+
+        }
+    }
+
+}
+
+
+
+
+
---
+++ Assets/Scripts/temaJogo.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class temaJogo : MonoBehaviour {
+    public Button  btnPlay;
+    public Text    txtNomeTema;
+    public GameObject infoTema;
+    public Text    txtInfoTema;
+    public GameObject estrela1;
+    public GameObject estrela2;
+    public GameObject estrela3;
+    public int numeroQuestoes;
+
+
+    public string[] nomeTema;
+    private int idTema;
+    // Use this for initialization
+    void Start () {
+        idTema = 0;
+        txtNomeTema.text = nomeTema[idTema];
+      
+        txtInfoTema.text = "Voce acertou x de x";
+        infoTema.SetActive(false);
+        estrela1.SetActive(false);
+        estrela2.SetActive(false);
+        estrela3.SetActive(false);
+        btnPlay.interactable = false;
+       
+
+}
+	
+	public void selecioneTema(int i)
+    {
+        idTema = i;
+        PlayerPrefs.SetInt("idTema", idTema);
+        txtNomeTema.text = nomeTema[idTema];
+
+        int notaFinal = PlayerPrefs.GetInt("notaFinal" + idTema.ToString());
+         int acertos = PlayerPrefs.GetInt("acertos" + idTema.ToString());
+
+        infoTema.SetActive(false);
+        estrela1.SetActive(false);
+        estrela2.SetActive(false);
+        estrela3.SetActive(false);
+
+        if (notaFinal == 10)
+        {
+            estrela1.SetActive(true);
+            estrela2.SetActive(true);
+            estrela3.SetActive(true);
+
+
+
+        }
+        else if (notaFinal >= 5)
+        {
+            estrela1.SetActive(true);
+            estrela2.SetActive(true);
+            estrela3.SetActive(false);
+        }
+        else if (notaFinal >= 1)
+        {
+            estrela1.SetActive(true);
+            estrela2.SetActive(false);
+            estrela3.SetActive(false);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+        txtInfoTema.text = " Voce acertou "+ acertos.ToString()+" de "+numeroQuestoes.ToString()+ " questões! ";
+
+        infoTema.SetActive(true);
+
+        btnPlay.interactable=true;
+
+
+
+
+    }
+    public void jogar()
+    {
+        Application.LoadLevel("T" + idTema.ToString());
+    }
+
+		
+	
+}
